54ac78b759d567612a621c086daa78ce63120fa5
Stefan Temme <temme@objectcode.de>
2022-12-01 23:13:13 +0100

    from O(n*log(n)) to O(n)

--- 01-04/01.js
+++ 01-04/01.js
@@ -6,9 +6,16 @@ const data = stardata;
 const allElves = data.split("\n\n")
     .map(elf => elf.split("\n")
         .reduce((a, b) => parseInt(b) + a, 0));
-allElves.sort((a, b) => Math.sign(b - a));
-console.log("⭐ Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?", allElves[0]);
+const sumTopN = (n)=>{
+    return allElves.reduce((topN,elf)=>{
+        topN.push(elf);
+        topN.sort((a, b) => Math.sign(b - a));
+        topN.length=Math.min(topN.length,n);
+        return topN;
+    },[]).reduce((a,b)=>a+b)
+}
 
-const firstThreeSum = allElves.reduce((a, b, i) => i < 3 ? a + b : a);
-console.log("⭐⭐ Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?", firstThreeSum);
+console.log("⭐ Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?", sumTopN(1));
+
+console.log("⭐⭐ Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?", sumTopN(3));
 
