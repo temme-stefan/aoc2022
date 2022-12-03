69611306a7045ffb5a8fd9dd8346410dcda8fb3f
Stefan Temme <temme@objectcode.de>
2022-12-03 08:48:25 +0100

    03 Star 2

--- 01-04/03.js
+++ 01-04/03.js
@@ -4,22 +4,27 @@ const data = stardata.split("\n");
 
 const getPriority= (c)=>c.toLowerCase()==c?(c.charCodeAt(0)-"a".charCodeAt(0)+1):(c.charCodeAt(0)-"A".charCodeAt(0)+27);
 
-const getDuplicate = (compartment1,compartment2)=> {
-    const {setA,setB} = [...compartment1].reduce(({setA, setB}, c, i) => {
-        setA.add(c);
-        setB.add(compartment2[i]);
-        return {setA, setB}
-    }, {
-        setA: new Set(),
-        setB: new Set()
-    });
-    return [...setA].find(c=>setB.has(c));
-}
+const getSet = (compartment)=>[...compartment].reduce((set,letter)=>{set.add(letter); return set;},new Set());
+
+const getDuplicate = (compartments)=>
+    [...compartments.map(getSet).reduce((a,b)=>new Set([...a].filter(c=>b.has(c))))][0];
+
 const getCompartments = (backpack)=>[backpack.substring(0,backpack.length/2),backpack.substring(backpack.length/2)];
 
-const prioritySum = data.map(backpack=>getPriority(getDuplicate(...getCompartments(backpack)))).reduce((a,b)=>a+b);
+const prioritySum = data.map(backpack=>getPriority(getDuplicate(getCompartments(backpack)))).reduce((a,b)=>a+b);
 
+const groups = data.reduce((gr,row)=>{
+    if(gr[0].length<3){
+        gr[0].push(row);
+    }
+    else{
+        gr.unshift([row]);
+    }
+    return gr;
+},[[]])
+
+const badgeSum = groups.map(rows=>getPriority(getDuplicate(rows))).reduce((a,b)=>a+b);
 
 console.log("⭐ Find the item type that appears in both compartments of each rucksack. What is the sum of the priorities of those item types?", prioritySum);
 
-console.log("⭐⭐ ");
+console.log("⭐⭐ Find the item type that corresponds to the badges of each three-Elf group. What is the sum of the priorities of those item types?",badgeSum);
