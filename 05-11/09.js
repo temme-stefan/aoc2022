b0cd07e8938d8ef43214937f92f4cc1edb0fe53e
Stefan Temme <temme@objectcode.de>
2022-12-09 09:45:42 +0100

    09 Star 2

--- 05-11/09.js
+++ 05-11/09.js
@@ -34,24 +34,23 @@ const moveHead = (head, direction) => {
  * @param tail {{x:number,y:number}}
  * @param head {{x:number,y:number}}
  */
-const moveTail = (tail,head) =>{
-    const dx = head.x-tail.x;
+const moveTail = (tail, head) => {
+    const dx = head.x - tail.x;
     const aDx = Math.abs(dx);
-    const dy = head.y-tail.y;
+    const dy = head.y - tail.y;
     const aDy = Math.abs(dy);
-    const move = aDx>1 || Math.abs(aDy>1);
-    if (move){
-        if (aDx==2){
-            tail.x+=Math.sign(dx);
-            if (dy!=0){
-                tail.y+=Math.sign(dy);
+    const move = aDx > 1 || Math.abs(aDy) > 1;
+    if (move) {
+        if (aDx === 2) {
+            tail.x += Math.sign(dx);
+            if (dy !== 0) {
+                tail.y += Math.sign(dy);
             }
-        }
-        else{
+        } else {
             //aDy==2
-            tail.y+=Math.sign(dy);
-            if (dx!=0){
-                tail.x+=Math.sign(dx);
+            tail.y += Math.sign(dy);
+            if (dx !== 0) {
+                tail.x += Math.sign(dx);
             }
         }
     }
@@ -60,25 +59,23 @@ const moveTail = (tail,head) =>{
 /**
  *
  * @param motions {{steps: number, direction: "R"|"L"|"U"|"D"}[]}
+ * @param ropeSize {number}
  * @returns {number}
  */
-const doMotions = (motions) => {
+const doMotions = (motions, ropeSize = 2) => {
     const trace = new Set();
 
-    const head = {
-        x: 0,
-        y: 0,
-    };
-
-    const tail = {x: 0, y: 0}
-
-    setVisit(tail, trace);
+    const knots = Array.from({length: ropeSize}).map(_ => {
+        return {x: 0, y: 0}
+    });
 
     motions.forEach(({direction, steps}) => {
         for (let i = 0; i < steps; i++) {
-            moveHead(head, direction);
-            moveTail(tail,head);
-            setVisit(tail,trace);
+            moveHead(knots[0], direction);
+            for (let j = 1; j < knots.length; j++) {
+                moveTail(knots[j], knots[j - 1]);
+            }
+            setVisit(knots[ropeSize - 1], trace);
         }
     })
     return trace.size;
@@ -94,8 +91,6 @@ const motions = data.split("\n").map(s => {
     }
 })
 
-const visitedByTail = doMotions(motions);
+console.log("⭐ Simulate your complete hypothetical series of motions. How many positions does the tail of the rope visit at least once?", doMotions(motions));
 
-console.log("⭐ Simulate your complete hypothetical series of motions. How many positions does the tail of the rope visit at least once?", visitedByTail);
-
-console.log("⭐⭐ ");
+console.log("⭐⭐ Simulate your complete series of motions on a larger rope with ten knots. How many positions does the tail of the rope visit at least once?", doMotions(motions, 10));
