3250f7d041c71de4cb3aa03ec9826604d239667f
Stefan Temme <temme@objectcode.de>
2022-12-13 19:41:57 +0100

    changed fromn bool? to -1,0,1 to satisfy sort...

--- 12-18/13.js
+++ 12-18/13.js
@@ -4,39 +4,38 @@ import {mulNumbers} from "../reusable/reducer.js";
 const parse = (data) =>
     data.split("\n\n").map(pair => pair.split("\n").map(row => JSON.parse(row)));
 
-let nested = 0
-
-function isOrdered(left, right) {
-    // console.log("".padStart(nested,"\t"),"Compare",left,"to",right);
-    nested++;
-    let valid = null;
+/**
+ *
+ * @param left
+ * @param right
+ * @returns {number}
+ */
+function compare(left, right) {
+    let valid = 0;
     const leftA = Array.isArray(left);
     const rightA = Array.isArray(right);
     if (leftA && rightA) {
         let i = 0;
-        for (; i < left.length && valid == null; i++) {
+        for (; i < left.length && valid == 0; i++) {
             if (i >= right.length) {
-                valid = false
-                // console.log("length break", valid);
+                valid = 1
                 break;
             }
-            valid = isOrdered(left[i], right[i]);
+            valid = compare(left[i], right[i]);
 
         }
-        if (valid == null && i == left.length && left.length < right.length) {
-            valid = true;
+        if (valid == 0 && i == left.length && left.length < right.length) {
+            valid = -1;
         }
     } else if (!leftA && !rightA) {
         if (left != right) {
-            valid = left < right;
-            // console.log("numberCheck",valid)
+            valid = left < right ? -1 : 1;
         }
     } else if (!leftA && rightA) {
-        valid = isOrdered([left], right)
+        valid = compare([left], right)
     } else {
-        valid = isOrdered(left, [right])
+        valid = compare(left, [right])
     }
-    nested--;
     return valid;
 }
 
@@ -44,7 +43,7 @@ const data = stardata;
 const parsed = parse(data);
 
 const orderedPairSum = parsed.reduce((s, [left, right], i) => {
-    if (isOrdered(left, right) ?? true) {
+    if (compare(left, right) == -1) {
         s += i + 1;
     }
     return s;
@@ -54,16 +53,6 @@ console.log("⭐ Determine which pairs of packets are already in the right order
 const allLines = parsed.flat();
 const extra = [[[2]], [[6]]]
 allLines.push(...extra);
-allLines.sort((a, b) => {
-    const result = isOrdered(a, b);
-    if (result) {
-        return -1;
-    }
-    if (result == null) {
-        return 0;
-    }
-    return 1;
-});
-
+allLines.sort(compare);
 const key = extra.map(k => allLines.indexOf(k) + 1).reduce(mulNumbers, 1);
 console.log("⭐⭐ Organize all of the packets into the correct order. What is the decoder key for the distress signal?", key);
