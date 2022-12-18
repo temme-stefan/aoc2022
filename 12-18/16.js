663a25c7d440aa142d5a91dc6be87aab9e6a67c4
Stefan Temme <temme@objectcode.de>
2022-12-18 16:42:37 +0100

    Day 16 Star 0

---
+++ 12-18/16.js
@@ -0,0 +1,9 @@
+import {example, stardata} from "./16-data.js";
+
+const data = example;
+const start = "AA";
+
+
+console.log("⭐ Work out the steps to release the most pressure in 30 minutes. What is the most pressure you can release?");
+
+console.log("⭐⭐ ");
