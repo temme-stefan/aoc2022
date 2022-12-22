e575752b671af49ccb5f54a03ce1a9ca46de37e1
Stefan Temme <temme@objectcode.de>
2022-12-22 12:54:49 +0100

    day 22 star 2

--- 19-25/22.js
+++ 19-25/22.js
@@ -77,46 +77,189 @@ const result = operations.reduce(({position, facing, map}, o) => {
 }, state)
 
 
-
 console.log("⭐ Follow the path given in the monkeys' notes. What is the final password?", passwort(result));
 
-const cubeSize = data == example?4:50;
 //Idea: manually cubing, walk over the edge also needs turning.
-/**
- *   1 2
- *   3
- * 4 5
- * 6
- *
- * 1=>2 right => right 0,99=>0,100 49,99=>49,100
- * 1=>3 regular down 49,50=>50,50 49,99=>50,99
- * 1=>4 left => right 0,50=>149,0 49,50=>100,0
- * 1=>6 up => right 0,50 => 150,0 0,99 =>199,0
- *
- * 2=>1 regular left
- * 2=>3 down =>left 49,100 => 50,50 49,149 => 99,50
- * 2=>5 right => left
- * 2=>6 up => up
- *
- * 3=>1 regular up
- * 3=>2 right => up 50,50 => 49,100 99,50 => 49,149
- * 3=>5 regular down
- * 3=>4 left => down
- *
- * 4=>1 left => right
- * 4=>3 up => right
- * 4=>5 regular right
- * 4=>6 regular down
- *
- * 5=>2 right => left
- * 5=>3 regular up
- * 5=>4 regular left
- * 5=>6 down => left
- *
- * 6=>1 left => down
- * 6=>2 down => down
- * 6=>4 regular up
- * 5=>6 right => up
- */
+const mapToCubeStar = (map) => {
+    const cube = map.map((r, i) => r.map((c, j) => {
+        if (c == " ") {
+            return null;
+        } else {
+            return {
+                value: c,
+                getNext: (dir) => {
+                    const atEdge = dir == 0 && j % 50 == 49
+                        || dir == 2 && j % 50 == 0
+                        || dir == 1 && i % 50 == 49
+                        || dir == 3 && i % 50 == 0;
+                    if (atEdge) {
+                        //   1 2
+                        //   3
+                        // 4 5
+                        // 6
+                        const blockRow = (i - i % 50) / 50;
+                        const blockCol = (j - j % 50) / 50;
+                        switch (true) {
+                            //1=>2 r=>r
+                            case dir == 0 && blockRow == 0 && blockCol == 1:
+                            //1=>3 d=>d
+                            case dir == 1 && blockRow == 0 && blockCol == 1:
+                            //2=>1 l=>l
+                            case dir == 2 && blockRow == 0 && blockCol == 2:
+                            //3=>5 d=>d
+                            case dir == 1 && blockRow == 1 && blockCol == 1:
+                            //3=>1 u=>u
+                            case dir == 3 && blockRow == 1 && blockCol == 1:
+                            //4=>5 r=>r
+                            case dir == 0 && blockRow == 2 && blockCol == 0:
+                            //4=>6 d=>d
+                            case dir == 1 && blockRow == 2 && blockCol == 0:
+                            //5=>4 l=>l
+                            case dir == 2 && blockRow == 2 && blockCol == 1:
+                            //5=>3 u=>u
+                            case dir == 3 && blockRow == 2 && blockCol == 1:
+                            //6=>4 u=>u
+                            case dir == 3 && blockRow == 3 && blockCol == 0:
+                                return {
+                                    pos: getNext(map, [i, j], dir),
+                                    dir
+                                }
 
-console.log("⭐⭐ ",state.map.length,state.map[0].length);
+                            //1=>4 l=>r
+                            case dir == 2 && blockRow == 0 && blockCol == 1:
+                                return {
+                                    pos: [149 - i, 0],
+                                    dir: 0
+                                }
+                            //1=>6 u=>r
+                            case dir == 3 && blockRow == 0 && blockCol == 1:
+                                return {
+                                    pos: [j + 100, 0],
+                                    dir: 0
+                                }
+
+                            //2=>5 r=>l
+                            case dir == 0 && blockRow == 0 && blockCol == 2:
+                                return {
+                                    pos: [149 - i, 99],
+                                    dir: 2
+                                }
+                            //2=>3 d=>l
+                            case dir == 1 && blockRow == 0 && blockCol == 2:
+                                return {
+                                    pos: [j - 50, 99],
+                                    dir: 2
+                                }
+                            //2=>6 u=>u
+                            case dir == 3 && blockRow == 0 && blockCol == 2:
+                                return {
+                                    pos: [199, j - 100],
+                                    dir: 3
+                                }
+
+                            //3=>2 r=>u
+                            case dir == 0 && blockRow == 1 && blockCol == 1:
+                                return {
+                                    pos: [49, i + 50],
+                                    dir: 3
+                                }
+                            //3=>4 l=>d
+                            case dir == 2 && blockRow == 1 && blockCol == 1:
+                                return {
+                                    pos: [100, i - 50],
+                                    dir: 1
+                                }
+
+                            //4=>1 l=>r
+                            case dir == 2 && blockRow == 2 && blockCol == 0:
+                                return {
+                                    pos: [149 - i, 50],
+                                    dir: 0
+                                }
+                            //4=>3 u=>r
+                            case dir == 3 && blockRow == 2 && blockCol == 0:
+                                return {
+                                    pos: [j + 50, 50],
+                                    dir: 0
+                                }
+
+                            //5=>2 r=>l
+                            case dir == 0 && blockRow == 2 && blockCol == 1:
+                                return {
+                                    pos: [149 - i, 149],
+                                    dir: 2
+                                }
+                            //5=>6 d=>l
+                            case dir == 1 && blockRow == 2 && blockCol == 1:
+                                return {
+                                    pos: [j + 100, 49],
+                                    dir: 2
+                                }
+
+                            //6=>5 r=>u
+                            case dir == 0 && blockRow == 3 && blockCol == 0:
+                                return {
+                                    pos: [149, i - 100],
+                                    dir: 3
+                                }
+                            //6=>2 d=>d
+                            case dir == 1 && blockRow == 3 && blockCol == 0:
+                                return {
+                                    pos: [0, j + 100],
+                                    dir: 1
+                                }
+                            //6=>1 l=>d
+                            case dir == 2 && blockRow == 3 && blockCol == 0:
+                                return {
+                                    pos: [0, i - 100],
+                                    dir: 1
+                                }
+
+                        }
+                    } else {
+                        return {
+                            pos: getNext(map, [i, j], dir),
+                            dir
+                        }
+                    }
+                }
+            }
+        }
+    }))
+    return cube;
+}
+
+const walkCube = (cube, position, facing, steps) => {
+    let canWalk = true;
+    while (canWalk && steps > 0) {
+        const current = cube[position[0]][position[1]];
+        const {pos: nextPos, dir: nextDir} = current.getNext(facing);
+        const lookAt = cube[nextPos[0]][nextPos[1]];
+        if (lookAt.value == ".") {
+            position = nextPos;
+            facing = nextDir;
+        } else {
+            canWalk = false;
+        }
+        steps--;
+    }
+    return {position, facing};
+}
+
+const cube = mapToCubeStar(state.map);
+
+const result2 = operations.reduce(({position, facing, map}, o) => {
+    if (Number.isInteger(o)) {
+        const next = walkCube(map, position, facing, o);
+        return {map, ...next};
+    } else {
+        if (o == "L") {
+            facing = (facing + 3) % 4;
+        } else {
+            facing = (facing + 1) % 4;
+        }
+    }
+    return {position, facing, map};
+}, {...state, map: cube})
+
+console.log("⭐⭐ Fold the map into a cube, then follow the path given in the monkeys' notes. What is the final password?", passwort(result2));
