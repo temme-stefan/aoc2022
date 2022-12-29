6c8f76cff7a4a62b42729dc09a25564c6b34c3c5
Stefan Temme <temme@objectcode.de>
2022-12-30 00:13:03 +0100

    day 18 Star 1

--- 12-18/18.js
+++ 12-18/18.js
@@ -1,7 +1,46 @@
-import {example, stardata} from "./18-data.js";
+import {example, example_xs, stardata} from "./18-data.js";
+import {sumNumbers} from "../reusable/reducer.js";
 
-const data = example;
+class P3{
+    static #lookup = new Map();
+    static #getKey = (a,b,c)=>`${a}_${b}_${c}`
+    static getP3 = (a,b,c)=>{
+        const key = P3.#getKey(a,b,c);
+        if (!P3.#lookup.has(key)){
+            P3.#lookup.set(key,new P3(a,b,c));
+        }
+        return P3.#lookup.get(key);
+    }
+    #a=0;
+    #b=0;
+    #c=0;
+    constructor(a,b,c) {
+        this.#a=a;
+        this.#b=b;
+        this.#c=c;
+    }
 
-console.log("⭐ ");
+    getAdjacent(){
+        return [[-1,0,0],[1,0,0],[0,-1,0],[0,1,0],[0,0,-1],[0,0,1]].map(([a,b,c])=>{
+            return P3.getP3(this.#a+a,this.#b+b,this.#c+c);
+        })
+    }
+
+    toString(){
+        return P3.#getKey(this.#a,this.#b,this.#c)
+    }
+}
+
+const parse = (data)=>{
+    return data.split("\n").map(r=>P3.getP3(...r.split(",").map(x=>parseInt(x))));
+}
+
+const data = stardata;
+
+const cubes = parse(data);
+const lookup = new Set(cubes);
+const surface = cubes.map(c=>c.getAdjacent().filter(a=>!lookup.has(a)).length).reduce(sumNumbers,0);
+
+console.log("⭐ What is the surface area of your scanned lava droplet?", surface);
 
 console.log("⭐⭐ ");
